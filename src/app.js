44bce00d052fce3c10129d02a7571e88fc2c463c
saurav kumar <sauravkumar91937@gmail.com>
2025-06-14 03:07:26 +0530

    final

--- src/app.js
+++ src/app.js
@@ -14,10 +14,14 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://aihealthcareassistent.netlify.app",
+    ],
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
