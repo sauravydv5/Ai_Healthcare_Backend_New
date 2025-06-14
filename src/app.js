451bbc351f1c3a0d9bcb97290566e92eb92b70e9
saurav kumar <sauravkumar91937@gmail.com>
2025-06-14 17:56:03 +0530

    backend add

--- src/app.js
+++ src/app.js
@@ -14,10 +14,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://aihealthcareassistent.netlify.app",
-    ],
+    origin: ["http://localhost:5173", "aihealthcareassistant.netlify.app"],
     credentials: true,
   })
 );
